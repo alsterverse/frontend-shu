4dfa117aa519ed10e21d53fea9ce090d642ed003
Johan Bergström <johan.bergstrom@isotop.se>
2023-05-03 11:12:31 +0200

    add sentry hook for the client

---
+++ src/hooks.client.ts
@@ -0,0 +1,11 @@
+import * as Sentry from '@sentry/sveltekit';
+
+Sentry.init({
+	dsn: import.meta.env.VITE_SENTRY_DSN,
+	tracesSampleRate: 1.0,
+	replaysSessionSampleRate: 0.1,
+	replaysOnErrorSampleRate: 1.0,
+	integrations: [new Sentry.Replay()],
+});
+
+export const handleError = Sentry.handleErrorWithSentry();
